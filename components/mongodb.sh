dcaf6f6089cc54fbf3342b8223f8ee29c6b0de9d
bluecollar-coder <anishkumarmalik@gmail.com>
2023-02-15 17:56:20 -0500

    updating ./roboshop-shell-script

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -1,1 +1,15 @@
-echo Install MongoDB component
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
+
+yum install -y mongodb-org
+systemctl enable mongod
+systemctl start mongod
+
+sed -e 's/127\.0\.0\.1/0\.0\.0\.0/gi' /etc/mongod.conf
+systemctl restart mongod
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+cd /tmp
+# -o added to below command to overwrite if unzip already exists. And hence this script can be run as many times as we need and will not fail
+unzip -o mongodb.zip
+cd mongodb-main
+mongo < catalogue.js
+mongo < users.js
